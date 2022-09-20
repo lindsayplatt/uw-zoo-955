89f873656a72f0d2fede7ce174d5b65d9162c14c
unknown <lplatt@usgs.gov>
2022-09-20 14:50:59 -0500

    answer the questions

--- tidyverse_homework.R
+++ tidyverse_homework.R
@@ -45,10 +45,23 @@ iris_means <- summarize(group_by(iris, Species), mn.petal.length = mean(Petal.Le
 
 # 5. pipes
 # the above can get unwieldy - rearrange iris_means from 4 using pipes
+iris_means <- iris %>% 
+  group_by(Species) %>% 
+  summarize(mn.petal.length = mean(Petal.Length))
+
 
 ## On Your Own #1 
 # now compute mean petal area for each species - how would you go about it using dplyr
+iris_mean_area <- iris %>% 
+  mutate(Petal.Area = Petal.Width * Petal.Length) %>% 
+  group_by(Species) %>% 
+  summarize(mn.petal.area = mean(Petal.Area))
+
 # Q: What is the mean petal area for each species
+# Species    mn.petal.area
+# setosa             0.366
+# versicolor         5.72 
+# virginica         11.3  
 
 # 6. arrange/select/count
 # determine which species has the longest mean petal length
@@ -61,14 +74,47 @@ iris_size <-
 
 # On Your Own #2
 # do the same for the other measurements (i.e. petal.width, sepal.length, etc)
+iris %>% 
+  # Tidy-er data format by pivoting
+  pivot_longer(cols = -Species, names_to = 'Measurement', values_to = 'Value') %>% 
+  # Find means for each Species first
+  group_by(Measurement, Species) %>% 
+  summarize(mean_value = mean(Value)) %>% 
+  # Now determine which species has the biggest mean
+  group_by(Measurement) %>% 
+  summarize(
+    biggest_value = max(mean_value),
+    biggest_value_row = which.max(mean_value), 
+    biggest_value_species = Species[which.max(mean_value)]) %>% 
+  select(Measurement, biggest_value_species, biggest_value)
+
 # Q: What is the mean petal and sepal lengths and widths for each species
+# Measurement  biggest_value_species biggest_value
+# Petal.Length virginica                      5.55
+# Petal.Width  virginica                      2.03
+# Sepal.Length virginica                      6.59
+# Sepal.Width  setosa                         3.43
 
 # count the number of records for each species
 (iris_spp_n <- count(iris, Species))
 
 # On Your Own #3
 # count the number of samples that are >= mean.petal.length for each species
+mean_petal_lengths <- iris %>% 
+  group_by(Species) %>% 
+  summarize(mean_petal_length = mean(Petal.Length))
+iris %>% 
+  select(Species, Petal.Length) %>% 
+  left_join(mean_petal_lengths) %>% 
+  filter(Petal.Length >= mean_petal_length) %>% 
+  group_by(Species) %>% 
+  tally()
+
 # Q: How many samples where Petal.Length >= mean.petal.length does each species have
+# Species        n
+# setosa        26
+# versicolor    27
+# virginica     25
 
 # 7. joins
 set.seed(123)
@@ -88,7 +134,39 @@ ht_wt <- left_join(ht, wt, by = "level")
 library(nycflights13)
 
 # Q: Which airport (name) had the greatest number of arriving flights in 2013?
+# Atlanta (ATL) had the greatest number of arriving flights in 2013
+flights %>% 
+  filter(year == 2013, !is.na(arr_time)) %>% 
+  left_join(select(airports, faa, name), by = c("dest" = "faa")) %>% 
+  group_by(name, dest) %>%
+  tally() %>% 
+  arrange(desc(n))
+
 # Q: Which airport (name) had the greatest number of delayed arriving flights?
+# Also Atlanta (ATL) here - has the greatest number of delayed flights
+flights %>% 
+  filter(year == 2013, !is.na(arr_delay)) %>% 
+  left_join(select(airports, faa, name), by = c("dest" = "faa")) %>% 
+  group_by(name, dest) %>%
+  tally() %>% 
+  arrange(desc(n))
+
 # Q: What is the manufacturer, model, year, and type of airplane that flew the 
 #    most flights in 2013 (only include planes with all relevant information)?
 #    How man flights was it?
+most_flights <- flights %>%  
+  filter(!is.na(tailnum)) %>% 
+  group_by(tailnum) %>% 
+  tally() %>% 
+  arrange(desc(n)) %>% 
+  head(10)
+planes %>% 
+  semi_join(most_flights) %>% 
+  left_join(most_flights) %>% 
+  arrange(desc(n)) %>% 
+  head(1) %>% 
+  select(tailnum, year, type, manufacturer, model, n_flights = n)
+
+# The most flights 
+# tailnum  year type                    manufacturer         model  n_flights
+#   1 N711MQ   1976 Fixed wing multi engine GULFSTREAM AEROSPACE G1159B       486
